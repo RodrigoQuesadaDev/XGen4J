22e1cebfdad10a27524be15ec50606e8c92331e0
Rodrigo Quesada <rodrigoquesada@gmail.com>
2015-06-21 21:12:05 -0600

    Added test for thrown excpetions' type.

--- src/test/java/com/rodrigodev/xgen/ExceptionsGeneratorTest.java
+++ src/test/java/com/rodrigodev/xgen/ExceptionsGeneratorTest.java
@@ -158,32 +158,91 @@ public class ExceptionsGeneratorTest {
     public void thrownExceptionsAreOfCorrectType() {
 
         assertThatThrownBy(() -> DirtyError
-                .throwException("Dinning Room"))
+                .throwException("ABCDE"))
                 .isExactlyInstanceOf(DirtyException.class);
 
         assertThatThrownBy(() -> TooSmallError
-                .throwException("Bathroom"))
+                .throwException("ABCDE"))
                 .isExactlyInstanceOf(TooSmallException.class);
 
-        error("BadSmell").description("The %s smells bad, really bad.", p(String.class, "roomName"));
-        error("Appliance").description("Something went wrong with %s appliance.", p(String.class, "applianceName"));
-        error("Damaged").description("%s has been damaged.", p(String.class, "applianceName"));
-        error("Discharged").description("%s is discharged.", p(String.class, "applianceName"));
-        error("Scratches").description("%s got some scratches.", p(String.class, "applianceName"));
-        error("FoodNotFound").description("Food %s not found! I'm starving.", p(String.class, "foodName"));
-        error("FrozenFood").description("%s is frozen! How could you possibly eat that?", p(String.class, "foodName"));
-        error("RottenFood").description("%s is rotten my friend.", p(String.class, "foodName"));
-        error("Oven").description("The oven is not working. Model: %s", p(String.class, "model"));
-        error("Undercooked").description("%s is undercooked my friend, I won't eat that.", p(String.class, "model"));
-        error("BurnedFood").description("Damn, %s just burned! We better order some pizza.", p(String.class, "model"));
-        error("TooMuchSmoke").description("Wow, there is too much smoke coming out from the oven. Do you think %s is okay?", p(String.class, "model"));
-        error("Bedroom").description("%'s bedroom is not a good place to sleep.", p(String.class, "personName"));
-        error("Bed").description("This bed is freaking bad. Model: %s", p(String.class, "model"));
-        error("PillowNotFound").description("There is no pillow! How can %s sleep here?", p(String.class, "personName"));
-        error("BedIsWarm").description("Bed #%d is too damn warm!", p(Integer.class, "bedNumber"));
-        error("BugsInfestation").description("Oh shoot, there is bug infestation here. We better move to %s.", p(String.class, "place"));
-        error("TooEarly").description("It's just %s, still to early to wake up.", p(String.class, "time"));
-        error("ShutUp").description("Shut up!, I just want to defeat the boss and get %s's legendary sword.", p(String.class, "legendaryHero"));
-        error("DidNotSound").description("Oh shooooot!!! Now WTF am I supposed to do? %s is going to fire me!", p(String.class, "bossName"));
+        assertThatThrownBy(() -> BadSmellError
+                .throwException("ABCDE"))
+                .isExactlyInstanceOf(BadSmellException.class);
+
+        assertThatThrownBy(() -> ApplianceError
+                .throwException("ABCDE"))
+                .isExactlyInstanceOf(ApplianceException.class);
+
+        assertThatThrownBy(() -> DamagedError
+                .throwException("ABCDE"))
+                .isExactlyInstanceOf(DamagedException.class);
+
+        assertThatThrownBy(() -> DischargedError
+                .throwException("ABCDE"))
+                .isExactlyInstanceOf(DischargedException.class);
+
+        assertThatThrownBy(() -> ScratchesError
+                .throwException("ABCDE"))
+                .isExactlyInstanceOf(ScratchesException.class);
+
+        assertThatThrownBy(() -> FoodNotFoundError
+                .throwException("ABCDE"))
+                .isExactlyInstanceOf(FoodNotFoundException.class);
+
+        assertThatThrownBy(() -> FrozenFoodError
+                .throwException("ABCDE"))
+                .isExactlyInstanceOf(FrozenFoodException.class);
+
+        assertThatThrownBy(() -> RottenFoodError
+                .throwException("ABCDE"))
+                .isExactlyInstanceOf(RottenFoodException.class);
+
+        assertThatThrownBy(() -> OvenError
+                .throwException("ABCDE"))
+                .isExactlyInstanceOf(OvenException.class);
+
+        assertThatThrownBy(() -> UndercookedError
+                .throwException("ABCDE"))
+                .isExactlyInstanceOf(UndercookedException.class);
+
+        assertThatThrownBy(() -> BurnedFoodError
+                .throwException("ABCDE"))
+                .isExactlyInstanceOf(BurnedFoodException.class);
+
+        assertThatThrownBy(() -> TooMuchSmokeError
+                .throwException("ABCDE"))
+                .isExactlyInstanceOf(TooMuchSmokeException.class);
+
+        assertThatThrownBy(() -> BedroomError
+                .throwException("ABCDE"))
+                .isExactlyInstanceOf(BedroomException.class);
+
+        assertThatThrownBy(() -> BedError
+                .throwException("ABCDE"))
+                .isExactlyInstanceOf(BedException.class);
+
+        assertThatThrownBy(() -> PillowNotFoundError
+                .throwException("ABCDE"))
+                .isExactlyInstanceOf(PillowNotFoundException.class);
+
+        assertThatThrownBy(() -> BedIsWarmError
+                .throwException(123))
+                .isExactlyInstanceOf(BedIsWarmException.class);
+
+        assertThatThrownBy(() -> BugsInfestationError
+                .throwException("ABCDE"))
+                .isExactlyInstanceOf(BugsInfestationException.class);
+
+        assertThatThrownBy(() -> TooEarlyError
+                .throwException("ABCDE"))
+                .isExactlyInstanceOf(TooEarlyException.class);
+
+        assertThatThrownBy(() -> ShutUpError
+                .throwException("ABCDE"))
+                .isExactlyInstanceOf(ShutUpException.class);
+
+        assertThatThrownBy(() -> DidNotSoundError
+                .throwException("ABCDE"))
+                .isExactlyInstanceOf(DidNotSoundException.class);
     }
 }
